7e77b9b90ec9d53dd9adc5e5403e54ed7848760f
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-08-05 14:17:48 +0800

    2025年8月5日

---
+++ python/1-test.py
@@ -0,0 +1,12 @@
+def Test(name):
+    """
+    This function takes a name as input and prints a greeting message.
+    
+    Parameters:
+    name (str): The name to greet.
+    
+    Returns:
+    None
+    """
+    print(f"Hello, {name}!")
+# 将当前的格子内容写入到 ../python/1-test.py 文件中
